fa55093ef44df452e9bca379dcdb25e64f494409
Tomoo Amano <amano@diverse-inc.com>
2016-12-16 10:43:55 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Hash::Tabular::Markdown - It's new $module
+
+# SYNOPSIS
+
+    use Hash::Tabular::Markdown;
+
+# DESCRIPTION
+
+Hash::Tabular::Markdown is ...
+
+# LICENSE
+
+Copyright (C) Tomoo Amano.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tomoo Amano <amano@diverse-inc.com>
---
+++ lib/Hash/Tabular/Markdown.pm
@@ -0,0 +1,39 @@
+package Hash::Tabular::Markdown;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Hash::Tabular::Markdown - It's new $module
+
+=head1 SYNOPSIS
+
+    use Hash::Tabular::Markdown;
+
+=head1 DESCRIPTION
+
+Hash::Tabular::Markdown is ...
+
+=head1 LICENSE
+
+Copyright (C) Tomoo Amano.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tomoo Amano E<lt>amano@diverse-inc.comE<gt>
+
+=cut
+
